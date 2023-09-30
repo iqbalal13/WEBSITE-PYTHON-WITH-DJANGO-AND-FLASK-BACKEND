0c275eb7e4b7b0f692bd334c0379c0572cd81c8f
iqbalal13 <123553063+iqbalal13@users.noreply.github.com>
2023-09-30 21:28:16 +0700

    Add files via upload

---
+++ PYTHON SQL/MYSQL DESCRIPE TABLE PERSON.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="iqbal",
+    database="testdatabase"
+
+)
+
+mycursor = db.cursor()
+
+#mycursor.execute("CREATE TABLE Person(name VARCHAR(50), age smallint UNSIGNED, personID int PRIMARY KEY AUTO_INCREMENT)")
+mycursor.execute("DESCRIBE person")
+
+for  x in mycursor:
+ print(x)
+
+
